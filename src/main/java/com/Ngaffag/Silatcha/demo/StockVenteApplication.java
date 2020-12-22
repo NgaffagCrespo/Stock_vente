0c31c7df01bf40fdf069797bdb1f70b35160f3fe
Ngaffag Crespo <theroypercy@gmail.com>
2020-12-22 23:46:03 +0100

    first commit

---
+++ src/main/java/com/Ngaffag/Silatcha/demo/StockVenteApplication.java
@@ -0,0 +1,13 @@
+package com.Ngaffag.Silatcha.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockVenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockVenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ngaffag/Silatcha/demo/StockVenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ngaffag.Silatcha.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockVenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
